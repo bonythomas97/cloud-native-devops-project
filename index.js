300d0f094a650998e0a49ec2166c50f372221372
Bony Thomas <106593638+bonythomas97@users.noreply.github.com>
2026-02-02 14:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 DevOps CI/CD Project is LIVE! Yes this is Bony Thomas');
+  res.send('🚀 DevOps CI/CD Project is LIVE! Yes this is Bony Thomas hello');
 });
 
 app.get('/health', (req, res) => {
